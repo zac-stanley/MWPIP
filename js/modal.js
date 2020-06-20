5f82fcec0f72b3f519d10a247d116bec4855ce18
Boyd <boyd.land@gmail.com>
2020-06-20 12:52:11 -0400

    example modal

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+//  for one modal
+
+let modalBtn = document.querySelector(".modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
